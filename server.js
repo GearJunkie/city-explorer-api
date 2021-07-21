ab044704fcb2dca96a568e0e64170038b9dcd53f
jesse dills <jessedills@yahoo.com>
2021-07-21 10:22:16 -0400

    forcastArr

--- server.js
+++ server.js
@@ -1,43 +1,40 @@
 'use strict';
 
+//-----My Dependencies-----//
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
 const weatherData = require('./data/weather.json');
 
+//-----My Config Server Stuff-----//
 dotenv.config();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001
 
-// app.listen(3000, () => console.log('server up'));
+//-----My Routes-----//
 
-// app.get('/test', (req, res) => {
-//   res.send('test worked');
-// })
-
-// app.listen(PORT, () => {
-//   console.log('server works on 3333');
-// });
-
+//-----Capture a request coming into localhost:3333/weather-----//
 app.get('/weather', (req, res) => {
-  const lat = req.query.lat;
-  const lon = req.query.lon;
-  const searchQuery = req.query.searchQuery;
-  const city = new Forecast(weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase()));
+  let lat = req.query.lat;
+  let lon = req.query.lon;
+  let searchQuery = req.query.searchQuery;
+  let city = (weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase()));
   
-
   class Forecast {
-    constructor(datetime, description) {
+    constructor(date, description) {
       this.date = date;
       this.description = description;
-      forecastArr = [];
-      // newArr = arr.map( ())
     }
   }
 
-  res(200).send(this.forecastArr);
+  let forecastArr = [];
+  city.data.map( (value, idx) => {
+    forecastArr.push(new Forecast(value.datetime, `A low of ${value.low_temp}, and a high of ${value.high_temp}, with ${value.weather.description}`))
+  })
+//-----Send a response back to the client with the forecast for the city they chose-----//
+  res.send(forecastArr);
 });
 
 app.listen(PORT, () => {
